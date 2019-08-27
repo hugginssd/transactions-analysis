a8fb531cdc39b6b9fde99961cee01b4a8c5d309c
hugginssd <hugginsmafigu@gmail.com>
2019-08-27 16:34:33 +0200

    Index js changes- routing

---
+++ src/cards/cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './styles';
+
+class CardComponent extends React.Component{
+
+    render(){
+        return(
+            <div>Hello from CardComponent</div>
+        );
+    }
+}
